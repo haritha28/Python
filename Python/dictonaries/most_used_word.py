c448d18473d188a4d983f377c6c24c52514c6524
Haritha28 <haritha.h20@gmail.com>
2017-02-27 18:20:08 +0530

    Most used word found

--- Python/dictonaries/most_used_word.py
+++ Python/dictonaries/most_used_word.py
@@ -9,13 +9,10 @@ listkey = list(count)
 print listkey
 listvalue = count.values()
 print listvalue
-max_used = most_frequent(count)
-
-def most_frequent(count):
-    most_frequent_value = 0
-    most_frequent_word = ''
-    for word in words:
-        if count[word] > most_frequent_value:
-            most_frequent_value = count[word]
-            most_frequent_word = word
-    return most_frequent_word
+mostfrequentvalue = 0
+mostfrequentword = ''
+for word in words:
+    if count[word] > mostfrequentvalue:
+        mostfrequentvalue = count[word]
+        mostfrequentword = word
+print mostfrequentword
